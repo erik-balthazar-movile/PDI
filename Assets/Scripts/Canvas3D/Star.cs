ee2345da7bd838cda0cb7043125c75d29f5515e5
Erik Balthazar <erik.balthazar@movile.com>
2015-08-07 16:21:24 -0300

    Delegate changed

--- Assets/Scripts/Canvas3D/Star.cs
+++ Assets/Scripts/Canvas3D/Star.cs
@@ -3,8 +3,22 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Star : MonoBehaviour {
-	public void PaintsRed() {
-		GetComponent<Image> ().color = Color.red;
+	private int index = 0;
+	private Color[] colors = {Color.magenta, Color.red, Color.black, Color.blue, Color.green, Color.grey};
+	delegate void MyPaintDelegate(Color[] c, int i);
+	MyPaintDelegate myPaintDelegate;
+
+	public void OnEnterStar() {
+		if (index >= colors.Length) {
+			index = 0;
+		}
+		myPaintDelegate = Paints;
+		myPaintDelegate (colors, index);
+		index++;
+	}
+
+	void Paints(Color[] c, int i) {
+		GetComponent<Image> ().color = c [i];
 	}
 
 	public void PaintsNormal() {
